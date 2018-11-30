c6ac95d7d9af23834a6c090dad0108d901612e67
IdoShapira <ishapira@yotpo.com>
2018-11-30 15:37:36 +0200

    Initial commit

---
+++ app/src/main/java/com/final_project_afeka/android_navigation_final_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.final_project_afeka.android_navigation_final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
