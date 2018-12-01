f87ee735457e866607f1f96fe090e9555293ccae
IdoShapira <ishapira@yotpo.com>
2018-12-01 16:22:06 +0200

    added google-services

--- app/src/main/java/com/final_project_afeka/android_navigation_final_project/MainActivity.java
+++ app/src/main/java/com/final_project_afeka/android_navigation_final_project/MainActivity.java
@@ -5,7 +5,6 @@ import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.FirebaseApp;
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
